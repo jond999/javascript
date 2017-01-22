72fafe161c7befcffb79fa7afa6c2605922da501
Henrique Abranches <jondhub999@gmail.com>
2017-01-22 13:56:21 +0000

    Revealing Module Pattern example file added!

---
+++ revealingModules.js
@@ -0,0 +1,43 @@
+
+function printableMessage()
+{
+	var message = 'hello';
+
+	function getMessage()
+	{
+		return message;
+	}
+
+	function setMessage(newMessage)
+	{
+		if(!newMessage)
+		{
+			throw new Error('Cannot set empty message!');
+		}
+
+		message = newMessage;
+	}
+
+	function printMessage()
+	{
+		console.log(message);
+	}
+
+	return {
+
+		setMessage: setMessage,
+		getMessage: getMessage,
+		printMessage: printMessage
+	};
+}
+
+// Pattern in use
+var awesome1 = printableMessage();
+awesome1.printMessage(); // hello
+
+var awesome2 = printableMessage();
+awesome2.setMessage('hi');
+awesome2.printMessage(); // hi
+
+console.log(awesome1.getMessage()); // hello
+console.log(awesome2.getMessage()); // hi
